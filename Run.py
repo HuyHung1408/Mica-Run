74bf43d45510ff7a5aa6f8d7a075cf5c5e1e6b26
HuyHung1408 <dunghuyhung1@gmail.com>
2022-02-28 19:12:55 +0700

    Add icon

--- Run.py
+++ Run.py
@@ -11,7 +11,8 @@ y_cordinate = int((run.winfo_screenheight() / 2) - (run.winfo_height() / 2))
 run.resizable(False, False)
 run.iconbitmap('Run icon.ico')
 
-image = BitmapImage('Run icon.ico')
+img = PhotoImage(file='Run icon.png')
+a = Label(run, image=img).grid(row= 1, column =1)
 text = ttk.Label(text='Type the name of a program, folder, document, or Internet\n resource, and Windows will open it for you', font='10').grid(row=1, column = 2)
 
 
