c4a3885615697c28a5838cc3123e715cf6d91397
HuyHung1408 <dunghuyhung1@gmail.com>
2022-03-06 16:37:38 +0700

    Push

--- Run.py
+++ Run.py
@@ -1,20 +1,36 @@
 from tkinter import ttk
 from tkinter import *
+from win32mica import ApplyMica, MICAMODE
+from ctypes import windll
 
 run = Tk()
 run.title('Run')
 run.geometry('399x173')
 run.update()
+
+Ok = ttk.Button(run, text='OK').grid(row=3, column=2)
+
+img = PhotoImage(file='Run icon.png')
+a = Label(run, image=img).grid(row= 1, column =1, padx=8, pady=8)
+text = ttk.Label(text='Type the name of a program, folder, document, or Internet\nresource, and Windows will open it for you.', font=('Segoe UI Variable Display','10')).grid(row=1, column = 2, pady=16)
+
+
+
+
 run.minsize(run.winfo_width(), run.winfo_height())
 x_cordinate = int((run.winfo_screenwidth() / 2) - (run.winfo_width() / 2))
 y_cordinate = int((run.winfo_screenheight() / 2) - (run.winfo_height() / 2))
 run.resizable(False, False)
 run.iconbitmap('Run icon.ico')
 
-img = PhotoImage(file='Run icon.png')
-a = Label(run, image=img).grid(row= 1, column =1)
-text = ttk.Label(text='Type the name of a program, folder, document, or Internet\n resource, and Windows will open it for you', font='10').grid(row=1, column = 2)
+run.tk.call("source", "sun-valley.tcl")
+run.tk.call("set_theme", "light")
 
-
+#App theme
+bg_color = ttk.Style().lookup(".", "background")
+run.wm_attributes("-transparent", bg_color)
+HWND=windll.user32.GetParent(run.winfo_id())
+ApplyMica(HWND, ColorMode=MICAMODE.LIGHT)
+run.update()
 
 run.mainloop()
