695247a70a54248fa22d203e8ee783d88338738b
HuyHung1408 <dunghuyhung1@gmail.com>
2022-03-08 19:50:01 +0700

    Changes

--- Run.py
+++ Run.py
@@ -1,8 +1,8 @@
-from cProfile import label
 from tkinter import ttk
 from tkinter import *
 from win32mica import ApplyMica, MICAMODE
 from ctypes import windll
+from tkinter import filedialog as fd
 
 #App name
 run = Tk()
@@ -10,23 +10,41 @@ run.title('Run')
 run.geometry('399x173')
 run.update()
 
-Ok = ttk.Button(run, text='OK').grid(row=4, column=2, ipadx=10, pady=10)
-Ok = ttk.Button(run, text='OK').grid(row=4, column=3, ipadx=10, pady=10)
-Ok = ttk.Button(run, text='OK').grid(row=4, column=4, ipadx=10, pady=10)
+def select_file():
+    filetypes = (
+        ('Text', '*.txt'),
+        ('All files', '*.*')
+    )
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
+    input = StringVar()
+    input.set(filename)
+
+Ok = ttk.Button(run, text='OK').place(x = 130, y = 130, width = 80)
+Cancel = ttk.Button(run, text='Cancel', command=run.destroy).place(x = 220, y = 130, width = 80)
+Browse = ttk.Button(run, text='Browse...', command=select_file).place(x = 310, y = 130, width = 80)
 
 opentext = ttk.Label(text='Open:').grid(row=3, column=1)
 
-input = ttk.Entry().grid(row=3, column=2, columnspan=3, ipadx=100, pady=2)
+input = ttk.Entry().grid(row=3, column=2, columnspan=4, ipadx=100, pady=2)
 
 img = PhotoImage(file='Run icon.png')
 Runicon = Label(run, image=img).grid(row= 1, column =1, padx=8, pady=8)
-text = ttk.Label(text='Type the name of a program, folder, document, or Internet\nresource, and Windows will open it for you.', font=('Segoe UI Variable Display','10')).grid(row=1, column = 2, columnspan=3, pady=16)
+text = ttk.Label(text='Type the name of a program, folder, document, or Internet\nresource, and Windows will open it for you.', font=('Segoe UI Variable Display','10')).grid(row=1, column = 2, columnspan=4, pady=16)
+
+
+filetypes = (
+        ('Text', '*.txt'),
+        ('All files', '*.*'))
 
 #App size
 run.minsize(run.winfo_width(), run.winfo_height())
 x_cordinate = int((run.winfo_screenwidth() / 2) - (run.winfo_width() / 2))
 y_cordinate = int((run.winfo_screenheight() / 2) - (run.winfo_height() / 2))
-run.resizable(True, True)
+run.resizable(False, False)
 run.iconbitmap('Run icon.ico')
 
 #Theme for Tkinter
